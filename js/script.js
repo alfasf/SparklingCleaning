4c1110576d30f83ccc75f8ceee56b9466c963d27
alfasf <diazalfredo83@gmail.com>
2017-05-21 12:45:39 -0700

    minimal adjustments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 	
-	$('.NavLogo').slideDown('slow');
 
-	$('h2').click(function(){
-		$(this).toggleClass('SecondH2');
-	});
-	
-	$('p').first().css('color', 'green');
-
-	$('.NavLogo').siblings().css("border", "2px solid red");
-	
-	$('div~img').fadeOut(3000);
 	
 });
